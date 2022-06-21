bb977ca248eb16f399a069570fd5e42c4d0cd6aa
RoxxxyCheater <sayt@3g.ua>
2022-06-21 16:33:49 +0300

    migrations: account, admin, auth, contenttypes, flatpages, news, sessions, sites, socialaccount

--- NewsPaper/protect/views.py
+++ NewsPaper/protect/views.py
@@ -5,3 +5,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(LoginRequiredMixin, TemplateView): # generic-представление для отображения шаблона унаследовав от LoginRequiredMixin
     template_name = '../templates/news.html'
+
+    def get_context_data(self, **kwargs): #переопределяем метод получения контекста
+        context = super().get_context_data(**kwargs) #получили весь контекст из класса-родителя
+        context['is_not_premium'] = not self.request.user.groups.filter(name = 'programmers').exists()
+        #добавили новую контекстную переменную is_no t_premium
+        #есть ли пользователь в группе - заходим в переменную запроса self.request/
+        #Из этой переменной мы можем вытащить текущего пользователя
+        #В поле groups хранятся все группы, в которых он состоит
+        #применяем фильтр к этим группам и ищем ту самую, имя которой premium.
+        #проверяем, есть ли какие-то значения в отфильтрованном списке.
+        #Mетод exists() вернёт True, если группа premium в списке групп пользователя найдена
+        #нам нужно получить наоборот — True, если пользователь не находится в этой группе, поэтому добавляем отрицание not
+        return context #возвращаем контекст обратно
--- NewsPaper/sign/views.py
+++ NewsPaper/sign/views.py
@@ -4,8 +4,20 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .models import BaseRegisterForm
 
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group #импортировать модель групп
+from django.contrib.auth.decorators import login_required #декоратор проверки аутентификации
+
 
 class BaseRegisterView(CreateView):
     model = User
     form_class = BaseRegisterForm
     success_url = '/news/'
+
+@login_required
+def upgrade_me(request):
+    user = request.user # получили объект текущего пользователя из переменной запроса
+    premium_group = Group.objects.get(name='programmers') #Вытащили premium-группу из модели Group
+    if not request.user.groups.filter(name='programmers').exists(): #проверяем, находится ли пользователь в этой группе (вдруг кто-то решил перейти по этому URL, уже имея Premium)
+        premium_group.user_set.add(user) #Если ещё не в ней — добавляем.
+    return redirect('/') # перенаправляем пользователя на корневую страницу, используя метод redirect
